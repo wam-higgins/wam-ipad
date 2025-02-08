8c0870da06123adc519665db1e4c7a4c11320bb4
bchabot <bchabot@gmail.com>
2025-02-08 15:56:01 -0500

    Update videofullscreen.js

--- videofullscreen.js
+++ videofullscreen.js
@@ -1,67 +1,52 @@
 // Code from AI generators.
 // The query was:
-// Write a javascript that works correctly on WKWebView on iPad that, when the user clicks on a video, will open that video in fullscreen, automatically play it, and when the video is done, will exit fullscreen.  Make sure it works correctly on iPad with WKWebView.
+// Write a javascript that works correctly on WKWebView on iPad that will play a separate preview clip from one of four full videos on the page, rotating which video has a preview playing, and when the user clicks on any video, will open that video in fullscreen, automatically play it, and when the video is done, will exit fullscreen.  Make sure it works correctly on iPad with WKWebView.
 
-/*
+
 // From https://zzzcode.ai/javascript/code-generator?id=9886521f-5ae3-4040-8bd6-d9b90f6b0265
 // Result: FAIL
-// Fullscreen on click: Pass
-// Auto Play: Pass
-// Close fullscreen on exit: Fail
+// Play Preview Clip: 
+// Rotate Clips playing correctly:
+// Fullscreen on click: 
+// Auto Play: 
+// Close fullscreen on exit: 
+const videos = document.querySelectorAll('video');
+/* 
+let currentPreviewIndex = 0;
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    const videos = document.querySelectorAll('video');
-
-    videos.forEach(video => {
-        video.addEventListener('click', function() {
-            if (video.requestFullscreen) {
-                video.requestFullscreen();
-            } else if (video.webkitRequestFullscreen) { // Safari
-                video.webkitRequestFullscreen();
-            } else if (video.msRequestFullscreen) { // IE11
-                video.msRequestFullscreen();
-            }
-            video.play();
-        });
-
-        video.addEventListener('ended', function() {
-            if (document.fullscreenElement) {
-                document.exitFullscreen();
-            }
-        });
-    });
-});
+function playPreview() {
+    videos[currentPreviewIndex].currentTime = 0;
+    videos[currentPreviewIndex].play();
+    currentPreviewIndex = (currentPreviewIndex + 1) % videos.length;
+    setTimeout(() => {
+        videos[currentPreviewIndex].pause();
+        playPreview();
+    }, 5000); // Change preview every 5 seconds
+}
 */
 
-/*
+videos.forEach((video) => {
+    video.addEventListener('click', () => {
+        video.requestFullscreen();
+        video.play();
+        video.onended = () => {
+            document.exitFullscreen();
+        };
+    });
+});
+
+playPreview();
+// End zzzcode
+
+
+
+
 // From https://www.codeconvert.ai/javascript-code-generator
 // Result: FAIL
 // Fullscreen on click: Pass
 // Auto Play: Pass
 // Close fullscreen on exit: Fail
 
-document.addEventListener('click', function(event) {
-    const video = event.target.closest('video');
-    if (video) {
-        if (video.requestFullscreen) {
-            video.requestFullscreen();
-        } else if (video.webkitRequestFullscreen) { // Safari
-            video.webkitRequestFullscreen();
-        }
-
-        video.play();
-
-        video.onended = function() {
-            if (document.exitFullscreen) {
-                document.exitFullscreen();
-            } else if (document.webkitExitFullscreen) { // Safari
-                document.webkitExitFullscreen();
-            }
-        };
-    }
-});
-*/
 
 
 // from https://workik.com/javascript-code-generator
@@ -71,48 +56,8 @@ document.addEventListener('click', function(event) {
 // Close fullscreen on exit: Fail
 // Requires ElementID, but best written
 
-// script.js
 
-document.addEventListener('DOMContentLoaded', function() {
-    const video = document.getElementById('myVideo');
 
-    // Function to toggle fullscreen
-    function openFullscreen(elem) {
-        if (elem.requestFullscreen) {
-            elem.requestFullscreen();
-        } else if (elem.mozRequestFullScreen) { // Firefox
-            elem.mozRequestFullScreen();
-        } else if (elem.webkitRequestFullscreen) { // Chrome, Safari and Opera
-            elem.webkitRequestFullscreen();
-        } else if (elem.msRequestFullscreen) { // IE/Edge
-            elem.msRequestFullscreen();
-        }
-    }
-
-    // Function to exit fullscreen
-    function closeFullscreen() {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.mozCancelFullScreen) { // Firefox
-            document.mozCancelFullScreen();
-        } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
-            document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) { // IE/Edge
-            document.msExitFullscreen();
-        }
-    }
-
-    // Event listener for video click
-    video.addEventListener('click', function() {
-        openFullscreen(video);
-        video.play(); // Automatically play the video
-    });
-
-    // Event listener for video ended
-    video.addEventListener('ended', function() {
-        closeFullscreen(); // Exit fullscreen when video is done
-    });
-});
 
 
 
